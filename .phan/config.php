907892c6193a6880768505fbdf7aed1a8b214cc6
Mark A. Hershberger <mah@nichework.com>
2023-10-21 23:07:23 -0400

    Tidy up the checks
    * Reduce the rules excluded by phpcs
    * Be a little bit more clever about phan

--- .phan/config.php
+++ .phan/config.php
@@ -10,15 +10,31 @@ $cfg['file_list'] = array_merge(
 	]
 );
 
-# Relative paths to other extensions that this one depends on:
-$otherExtensions = [ 'vendor/nichework/tag-builder' ];
+$tagLib = "vendor/nichework/tag-builder";
 
-$cfg['directory_list'] = array_merge(
-	$cfg['directory_list'], $otherExtensions, [ realpath( getenv( "MW_INSTALL_PATH" ) ) ]
-);
+# Paths (either absolute or relative to the top level of this extension) to other extensions
+# that this one depends on:
+$otherPaths = [];
 
+$mwDir = getenv( "MW_INSTALL_PATH" );
+if ( $mwDir === false ) {
+	throw new Exception( "Please set MW_INSTALL_PATH!" );
+}
+$otherPaths[] = $mwDir;
+
+if ( !file_exists( "$mwDir/$tagLib" ) ) {
+	$otherPaths[] = $tagLib;
+}
+
+$cfg['directory_list'] = array_merge( $cfg['directory_list'], $otherPaths );
+
+$otherPaths[] = "node_modules";
+$extDir = "$mwDir/extensions/" . basename( dirname( __DIR__ ) );
+if ( file_exists( $extDir ) ) {
+	$otherPaths[] = $extDir;
+}
 $cfg['exclude_analysis_directory_list'] = array_merge(
-	$cfg['exclude_analysis_directory_list'], $otherExtensions, [ realpath( getenv( "MW_INSTALL_PATH" ) ), 'node_modules' ]
+	$cfg['exclude_analysis_directory_list'], $otherPaths
 );
 
 # Put messages you want to suppress here.
